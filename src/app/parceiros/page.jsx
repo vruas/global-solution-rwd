3e79250ec167c444121078236bbbef7672280bfe
vruas <vitor-ruas@live.com>
2023-11-22 17:01:19 -0300

    criando página de empresa parceira

---
+++ src/app/parceiros/page.jsx
@@ -0,0 +1,41 @@
+import React from "react";
+
+export default function Parceiros() {
+  return (
+    <main class="container mx-auto px-4 py-8">
+  <section class="mb-8">
+    <h2 class="text-3xl font-bold mb-4">Conheça mais sobre a <a href="https://www.hapvidandi.com.br/"><span className="text-red-500 hover:text-red-800 transition-all ease-out" >Hapvida NotreDame Intermédica</span></a> </h2>
+    <p class="mb-4">
+      A Hapvida NotreDame Intermédica é, hoje, a maior operadora de saúde do Brasil, com mais de 16,1 milhões de beneficiários de saúde e odontologia. Fruto da fusão entre o Hapvida, que começou suas operações em 1979 com a inauguração do Hospital Antonio Prudente, e a NotreDame Intermédica, fundada em 1968 e pioneira em Medicina Preventiva no País, oferece saúde integral e odontologia para Clientes empresariais e individuais em todas as regiões: Norte, Nordeste, Centro-Oeste, Sudeste e Sul.
+    </p>
+    <p class="mb-4">
+      Conta com rede assistencial própria composta por 85 hospitais, 77 prontos atendimentos e 331 clínicas médicas. Também dispõe aos seus beneficiários 271 centros de diagnóstico por imagem e coleta laboratorial, dos quais cerca de 100 operações fazem parte do Laboratório NotreLabs, uma rede nacional com capacidade de processar 4,5 milhões de análises clínicas e 500 mil exames de imagens por mês.
+    </p>
+    <p class="mb-4">
+      A operadora pensa em cada etapa do atendimento médico. Por isso, tem como foco oferecer sempre a melhor experiência: agendamento prático de consultas, atendimento humanizado e Medicina Preventiva, bem como uma gestão eficiente da saúde dos colaboradores das empresas Clientes.
+    </p>
+    <p>
+      Como parte de sua estratégia, a Hapvida NotreDame Intermédica também conta com um time especializado e dedicado que cuida dos aspectos Ambientais, Sociais e de Governança (ASG), buscando perpetuar a sustentabilidade do negócio. Em 2021, por exemplo, pelo segundo ano consecutivo a empresa foi considerada “carbono neutro” por seu trabalho para a compensação de 100% das emissões. Além disso, os beneficiários, dentistas, corretores e corpo clínico da Hapvida NotreDame Intermédica têm acesso a clubes de vantagens e descontos em drogarias e parceiros dos segmentos de compras on-line, educação e cursos, entretenimento, saúde e bem-estar, comércio, serviços, viagens e produtos odontológicos.
+    </p>
+  </section>
+  
+  <section class="mb-8">
+    <h2 class="text-3xl font-bold mb-4">Missão</h2>
+    <p class="mb-4">Proporcionar saúde integrada de qualidade, acessível a geração de brasileiros.</p>
+
+    <h2 class="text-3xl font-bold mb-4">Valores</h2>
+
+    <ul class="flex justify-center gap-4">
+      <li>Acolhimento</li>
+      <li>Comprometimento</li>
+      <li>Ética e combate à corrupção</li>
+      <li>Inovação e aprendizado</li>
+      <li>Respeito</li>
+      <li>Responsabilidade social</li>
+      <li>Sustentabilidade</li>
+      <li>Eficiência em custo</li>
+    </ul>
+  </section>
+</main>
+  );
+}
